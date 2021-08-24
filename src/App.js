a6208fef171b68c1e8258463c5341c395c988c1b
Harsányi Péter Kristóf <di2e6a@inf.elte.hu>
2021-08-24 02:06:03 +0200

    Implemented menu

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
-      </header>
+      <div className="Title">Pisont</div>
+      <div className="Buttons">
+        <div>Create Room</div>
+        <div>Connect</div>
+        <div>Rules</div>
+      </div>
     </div>
   );
 }
